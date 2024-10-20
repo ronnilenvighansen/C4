1dde796776d78a220936f5bb7dcbfa8cc209de28
ronnilenvighansen <ronnilenvighansen@gmail.com>
2024-10-20 13:13:22 +0100

    Add files via upload

---
+++ MyLoggingApi/Program.cs
@@ -0,0 +1,36 @@
+using Microsoft.Data.Sqlite;
+using MyLoggingApi.Logging;
+
+var builder = WebApplication.CreateBuilder(args);
+
+var connectionString = "Data Source=logs.db";
+
+using (var connection = new SqliteConnection(connectionString))
+{
+    connection.Open();
+    var command = connection.CreateCommand();
+    command.CommandText = @"
+        CREATE TABLE IF NOT EXISTS Logs (
+            Id INTEGER PRIMARY KEY AUTOINCREMENT,
+            LogLevel TEXT,
+            Message TEXT,
+            Timestamp TEXT
+        )";
+    command.ExecuteNonQuery();
+}
+
+builder.Services.AddLogging(loggingBuilder =>
+{
+    loggingBuilder.ClearProviders();
+    loggingBuilder.AddProvider(new SQLiteLoggerProvider(connectionString));
+});
+
+builder.Services.AddControllers();
+
+var app = builder.Build();
+
+app.UseRouting();
+app.UseAuthorization();
+app.MapControllers();
+
+app.Run();
